f4e07a0ed8b04bdbcc933d67c22e5fe58229af14
BasmalaBrham <basmala.brham@gmail.com>
2024-11-23 22:31:30 +0200

    Added price range filter to allow users to sort products within a specified price range

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -16,6 +16,8 @@ class ShopController extends Controller
         $order = $request->query('order') ?? -1;
         $f_brands=$request->query('brands');
         $f_categories=$request->query('categories');
+        $min_price=$request->query('min')??1;
+        $max_price=$request->query('max')??500;
         switch ($order) {
             case 1:
                 $o_column = 'created_at';
@@ -46,10 +48,13 @@ class ShopController extends Controller
         })
         ->when($categoriesFilter, function ($query) use ($categoriesFilter) {
             $query->whereIn('category_id', $categoriesFilter);
+        })->where(function($query) use($min_price,$max_price){
+            $query->whereBetween('regular_price',[$min_price,$max_price])
+            ->orWhereBetween('sale_price',[$min_price,$max_price]);
         })
         ->orderBy($o_column, $o_order)
         ->paginate($size);
-        return view('shop',compact('products','size','order','brands','f_brands','categories','f_categories'));
+        return view('shop',compact('products','size','order','brands','f_brands','categories','f_categories','max_price','min_price'));
     }
 
     public function productDetails($product_slug){
